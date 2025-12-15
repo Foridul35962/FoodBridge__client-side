2dff1dde08d1381b570dbbe4377ef2daf76dfede
Foridul35962 <foridulislam35962@gmail.com>
2025-12-16 02:04:45 +0600

    some router added for reset password

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Registration from './pages/Registration'
 import UserLayout from './layouts/UserLayout'
+import ForgetPass from './pages/ForgetPass'
 
 const App = () => {
 
@@ -23,6 +24,10 @@ const App = () => {
         {
           path: '/registration',
           element : <Registration />
+        },
+        {
+          path: '/forget-pass',
+          element: <ForgetPass />
         }
       ]
     }
