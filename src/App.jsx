c00a5b18d5e80c9cbacc2a3c5a165da82d5ff928
Foridul35962 <foridulislam35962@gmail.com>
2025-12-15 21:44:43 +0600

    user route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Registration from './pages/Registration'
+import UserLayout from './layouts/UserLayout'
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <UserLayout />,
+      children:[
+        {
+          path:'/',
+          element: <Home/>
+        },
+        {
+          path: '/login',
+          element: <Login />
+        },
+        {
+          path: '/registration',
+          element : <Registration />
+        }
+      ]
+    }
+  ])
   return (
-    <div className='text-red-600'>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
