4ef0851a1ee8a060dab27d0d142b417abe13dcca
Foridul35962 <foridulislam35962@gmail.com>
2025-12-18 00:47:05 +0600

    auth wise page route added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Loading from '../components/Loading'
+import UserLayout from '../layouts/UserLayout'
+import OwnerLayout from '../layouts/OwnerLayout'
+import DeliveryLayout from '../layouts/DeliveryLayout'
 
 const Home = () => {
+  const { loading, user } = useSelector((state) => state.auth)
   return (
-    <div>Home</div>
+    <>
+      {
+        loading ? <Loading /> :
+          (user?.role === 'user' ? <UserLayout /> :
+            (user?.role === 'owner' ? <OwnerLayout /> :
+              user?.role === 'deliveryBoy' && <DeliveryLayout />)
+          )
+      }
+    </>
   )
 }
 
