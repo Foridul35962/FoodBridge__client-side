9686ad28449b0427024184292e552a1fe2bdfa42
Foridul35962 <foridulislam35962@gmail.com>
2025-12-17 22:46:39 +0600

    debug route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import React from 'react'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Registration from './pages/Registration'
-import UserLayout from './layouts/UserLayout'
+import MainLayout from './layouts/MainLayout'
 import ForgetPass from './pages/ForgetPass'
 import { ToastContainer } from 'react-toastify'
 import useGetCity from './hooks/UseGetCity'
 import UseGetUser from './hooks/UseGetUser'
-import { useSelector } from 'react-redux'
-
-const App = () => {
-
-  //fetchUser
-  UseGetUser()
-  useGetCity()
-  const { user } = useSelector((state) => state.auth)
 
 
-  const router = createBrowserRouter([
+
+const router = createBrowserRouter([
     {
       path: '/',
-      element: <UserLayout />,
+      element: <MainLayout />,
       children: [
         {
           path: '/',
@@ -29,19 +22,26 @@ const App = () => {
         },
         {
           path: '/login',
-          element: user ? <Navigate to='/' /> : <Login />
+          element:  <Login />
         },
         {
           path: '/registration',
-          element: user ? <Navigate to='/' /> : <Registration />
+          element: <Registration />
         },
         {
           path: '/forget-pass',
-          element: user ? <Navigate to='/' /> : <ForgetPass />
+          element: <ForgetPass />
         }
       ]
     }
   ])
+
+const App = () => {
+
+  //fetchUser
+  UseGetUser()
+  useGetCity()
+
   return (
     <>
       <ToastContainer />
