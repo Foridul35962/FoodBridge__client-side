48e8ebc8055ae0cd1e8f1730867f08697ee77b51
Foridul35962 <foridulislam35962@gmail.com>
2025-12-18 13:12:08 +0600

    get full address

--- src/hooks/UseGetCity.jsx
+++ src/hooks/UseGetCity.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCity } from "../store/slice/authSlice";
+import { setAddress, setCity, setState } from "../store/slice/authSlice";
 
 function useGetCity() {
     const dispatch = useDispatch()
@@ -17,7 +17,11 @@ function useGetCity() {
             const latitude = position.coords.latitude
             const longitude = position.coords.longitude
             const location = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${import.meta.env.VITE_GEOAPI_KEY}`)
+            const address = location?.data?.results[0]?.address_line2
+            const state = location?.data?.results[0]?.state
             const city = location?.data?.results[0]?.city
+            dispatch(setAddress(address))
+            dispatch(setState(state))
             dispatch(setCity(city))
         })
     }, [user])
