da628084fab56111698f6a8a779e4a09b1425e18
Foridul35962 <foridulislam35962@gmail.com>
2025-12-20 17:31:44 +0600

    change background color

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import LoadingVideo from '../assets/Loading.webm'
 
 const Loading = () => {
     return (
-        <div className='bg-white text-4xl flex items-center justify-center h-screen w-full'>
+        <div className='bg-orange-50 text-4xl flex items-center justify-center h-screen w-full'>
             <video muted autoPlay loop src={LoadingVideo}></video>
         </div>
     )
