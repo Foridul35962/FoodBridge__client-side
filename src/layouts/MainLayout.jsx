c3fb9222324c909dc36ed1ecff41d282362e5d10
Foridul35962 <foridulislam35962@gmail.com>
2025-12-20 17:33:10 +0600

    add auto scroll to top for show new page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
-      <div className='w-full flex flex-col bg-orange-50'>
-        <Header />
-        {
-            <Outlet />
-        }
-        <Footer />
+    <div className='w-full flex flex-col bg-orange-50'>
+      <Header />
+      {
+        <Outlet />
+      }
+      <Footer />
     </div>
   )
 }
