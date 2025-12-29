934e1d100fbbaf34aeb0660d0ccac9137b4443e9
Foridul35962 <foridulislam35962@gmail.com>
2025-12-29 20:46:58 +0600

    debug

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -4,26 +4,28 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getMyOrders } from '../store/slice/orderSlice'
 import UserOrderCart from '../components/pages/UserOrderCart'
 import OwnerOrderCart from '../components/pages/OwnerOrderCart'
+import DeliveryBoyOrderCart from '../components/pages/DeliveryBoyOrderCart'
+import Loading from '../components/Loading'
 
 const MyOrders = () => {
     const dispatch = useDispatch()
-    const { orders } = useSelector((state) => state.order)
+    const { orders, orderLoading } = useSelector((state) => state.order)
     const { user } = useSelector((state) => state.auth)
     useEffect(() => {
-        const getMyOrder = async()=>{
-            try {
-                await dispatch(getMyOrders()).unwrap()
-            } catch (error) {
-                console.log(error)
-            }
+        if (!user) return
+
+        if (user.role !== 'deliveryBoy') {
+            dispatch(getMyOrders())
         }
-        getMyOrder()
-    }, [])
+    }, [dispatch, user])
+
     return (
         <>
             {
-                user?.role === 'user' && Array.isArray(orders) ? <UserOrderCart orders={orders}/> :
-                    user?.role === 'owner' && <OwnerOrderCart orders={orders} />
+                orderLoading ? <Loading /> :
+                    user?.role === 'user' && Array.isArray(orders) ? <UserOrderCart orders={orders} /> :
+                        user?.role === 'owner' ? <OwnerOrderCart orders={orders} /> :
+                            user?.role === 'deliveryBoy' && <DeliveryBoyOrderCart />
             }
         </>
     )
